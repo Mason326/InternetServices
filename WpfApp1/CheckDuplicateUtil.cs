f691c093b59c9149db7ca293509e8ddc90ec8eff
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2025-10-26 15:39:09 +0300

    Duplicate checking function is done

---
+++ WpfApp1/CheckDuplicateUtil.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class CheckDuplicateUtil
+    {
+        public static bool HasNoDuplicate(string tableName, string fieldName, string inputValue)
+        {
+            try
+            {
+                string[] removedMultipleSpacesArray = inputValue.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string removedMultipleSpaces = string.Join(" ", removedMultipleSpacesArray);
+                string trimmedInputValue = removedMultipleSpaces.Trim();
+                using (MySqlConnection conn = new MySqlConnection(Connection.ConnectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand($"Select * from `{tableName}` where trim(`{fieldName}`) = '{trimmedInputValue}'", conn);
+                    object duplicateId = cmd.ExecuteScalar();
+                    if (duplicateId != null)
+                        return false;
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
