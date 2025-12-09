7b46690de08b4f3d9a920096503d14528d5da8fa
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2025-12-09 22:32:40 +0300

    Added records counter class

---
+++ WpfApp1/RecordsCounter.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class RecordsCounter
+    {
+        public static int CountRecords(string tableName)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Connection.ConnectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand($"Select Count(*) from `{tableName}`", conn);
+                    object countRecords = cmd.ExecuteScalar();
+                    if (countRecords != null)
+                        return Convert.ToInt32(countRecords);
+                    else
+                        return 0;
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
