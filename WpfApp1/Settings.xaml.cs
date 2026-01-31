57cad1b57fcb71ef670f2fa0b721d9672b6e7b70
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2026-01-31 13:31:39 +0300

    Test connection is added

--- WpfApp1/Settings.xaml.cs
+++ WpfApp1/Settings.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace WpfApp1
 {
@@ -27,6 +28,34 @@ namespace WpfApp1
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private async void TestConnectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            string server = ServerTextbox.Text.Trim();
+            string user = UserTextbox.Text.Trim();
+            string password = PasswordTextbox.Password.Trim();
+            string db = Properties.Settings.Default.database;
+
+            if (string.IsNullOrEmpty(server) || string.IsNullOrEmpty(user))
+            {
+                MessageBox.Show($"Необходимо заполнить поля помеченные \"*\"", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection($"server={server};user={user};password={password};database={db}"))
+                {
+                    var taskConnect = conn.OpenAsync();
+                    await taskConnect;
+                    MessageBox.Show("Соединение установлено успешно", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show($"Ошибка подключения: {exc.Message}", "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
