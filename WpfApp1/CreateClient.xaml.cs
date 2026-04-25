b6a7fd8407e081f203b80f5a984e833cd3cb72be
Mikhail Poliakov <mikhail.polyakov.2006@bk.ru>
2026-04-25 14:17:31 +0300

    Hidding data in Client form is added

--- WpfApp1/CreateClient.xaml.cs
+++ WpfApp1/CreateClient.xaml.cs
@@ -699,10 +699,10 @@ namespace WpfApp1
             using (MySqlConnection conn = new MySqlConnection(Connection.ConnectionString))
             {
                 conn.Open();
-                string cmdText = $@"Select idclient, full_name, email, phone_number, place_of_residence, birthdate, subscriber_login, subscriber_password, passport_series, passport_number, issued_by, issue_date, department_code, client_status.status_name as 'client_status' from `client` inner join `client_status` on `client`.client_status_id = client_status.idclient_status  {filterOption} order by idclient desc;";
+                string cmdText = $@"Select idclient, full_name, email, phone_number, place_of_residence, birthdate, subscriber_login, subscriber_password, passport_series, passport_number, issued_by, issue_date, department_code, concat('ФИО: ', full_name, '\nEmail: ', IFNULL(email, ''), '\nТелефон: ', concat('+7 (***) ***', substring(phone_number, 13)), '\nАдрес проживания: ', place_of_residence, '\nДата рождения: ', REPEAT('*', CHAR_LENGTH(birthdate)), '\nСерия паспорта: ', concat('**', substring(passport_series, 3)), '\nНомер паспорта: ', concat('****', substring(passport_number, 5))) as clientDetails, client_status.status_name as 'client_status' from `client` inner join `client_status` on `client`.client_status_id = client_status.idclient_status {filterOption} order by idclient desc;";
                 if (isInitial)
                 {
-                    cmdText = $@"Select idclient, full_name, email, phone_number, place_of_residence, birthdate, subscriber_login, subscriber_password, passport_series, passport_number, issued_by, issue_date, department_code, client_status.status_name as 'client_status' from `client` inner join `client_status` on `client`.client_status_id = client_status.idclient_status order by full_name;";
+                    cmdText = $@"Select idclient, full_name, email, phone_number, place_of_residence, birthdate, subscriber_login, subscriber_password, passport_series, passport_number, issued_by, issue_date, department_code, concat('ФИО: ', full_name, '\nEmail: ', IFNULL(email, ''), '\nТелефон: ', concat('+7 (***) ***', substring(phone_number, 13)), '\nАдрес проживания: ', place_of_residence, '\nДата рождения: ', REPEAT('*', CHAR_LENGTH(birthdate)), '\nСерия паспорта: ', concat('**', substring(passport_series, 3)), '\nНомер паспорта: ', concat('****', substring(passport_number, 5))) as clientDetails, client_status.status_name as 'client_status' from `client` inner join `client_status` on `client`.client_status_id = client_status.idclient_status order by full_name;";
                 }
                 MySqlCommand cmd = new MySqlCommand(cmdText, conn);
                 MySqlDataAdapter da = new MySqlDataAdapter(cmd);
@@ -788,7 +788,7 @@ namespace WpfApp1
                 issuedByTextBox.Text = fieldValuesOfARecord[10].ToString().Trim();
                 issueDate.SelectedDate = DateTime.Parse(((DateTime)fieldValuesOfARecord[11]).ToString("dd.MM.yyyy"));
                 departmentCodeTextBox.Text = fieldValuesOfARecord[12].ToString().Trim();
-                clientStatusCombobox.Text = fieldValuesOfARecord[13].ToString().Trim();
+                clientStatusCombobox.Text = fieldValuesOfARecord[14].ToString().Trim();
 
                 searchByPassportSeriesAndNumber.IsEnabled = false;
                 clientStatusCombobox.IsEnabled = true;
